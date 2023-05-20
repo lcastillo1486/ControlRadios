38a8b884e49e0618c65f7f67b5587ef1b5ec3aa0
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-05-20 16:38:54 -0500

    aumentodemon

--- cliente/models.py
+++ cliente/models.py
@@ -14,7 +14,7 @@ class cliente(models.Model):
         return self.nombre
 
 class razonSocial(models.Model):
-    denominacion = models.CharField(max_length=50)
+    denominacion = models.CharField(max_length=150)
     ruc = models.CharField(max_length=11, null= True, blank= True)
     direccion = models.TextField(blank=True, null= True)
     email = models.EmailField(blank=True, null=True)
