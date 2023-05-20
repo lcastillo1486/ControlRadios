c380e1499ae66c6db0a2db81ede614f9379e05f3
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-05-20 16:23:38 -0500

    CmabioModels

--- cliente/models.py
+++ cliente/models.py
@@ -5,8 +5,8 @@ class cliente(models.Model):
     
     ruc = models.CharField(max_length=11, null= True, blank= True)
     nombre = models.CharField(max_length=100)
-    direccion = models.TextField(blank=True)
-    email = models.EmailField(blank=True)
+    direccion = models.TextField(blank=True, null= True)
+    email = models.EmailField(blank=True, null= True)
     telefono = models.PositiveBigIntegerField(max_length=9,blank=True, null = True)
     persona_contacto = models.CharField(max_length = 20, null = True, blank = True)
 
@@ -16,8 +16,8 @@ class cliente(models.Model):
 class razonSocial(models.Model):
     denominacion = models.CharField(max_length=50)
     ruc = models.CharField(max_length=11, null= True, blank= True)
-    direccion = models.TextField(blank=True)
-    email = models.EmailField(blank=True)
+    direccion = models.TextField(blank=True, null= True)
+    email = models.EmailField(blank=True, null=True)
     telefono = models.PositiveBigIntegerField(max_length=9,blank=True, null = True)
     persona_contacto = models.CharField(max_length = 20, null = True, blank = True)
 
