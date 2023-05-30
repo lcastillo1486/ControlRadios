d6a537a2b7934458723073513e6b759a6748070d
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-05-30 17:06:47 -0500

    logordenes

--- ordenes/views.py
+++ ordenes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse, redirect
 from .forms import formOrden, formEdit
 from django.contrib import messages
 from .models import ordenRegistro
+from movimientos.models import auditoria
+from django.utils import timezone
 # Create your views here.
 
 
@@ -16,6 +18,19 @@ def ordenes(request):
             a = form.save(commit=False)
             a.estado_id = 2
             a.save()
+
+            #buscar el ultimo id generado
+            ultimo_id = ordenRegistro.objects.latest('id').id
+            #capturar usuario actual
+            user_nombre = request.user.username
+            #fecha y hora actual
+            fecha_hora_peru = timezone.localtime(timezone.now())
+            fecha_hora_formateada = fecha_hora_peru.strftime('%Y-%m-%d %H:%M:%S')
+
+            log = auditoria(fecha = fecha_hora_formateada, accion = f'Orden Creada N° {ultimo_id}', usuario = user_nombre)
+            log.save()
+
+
             
             return render(request, 'ordenes.html', context)
         else:
