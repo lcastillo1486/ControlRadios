100e4e902c366a3b99b10ff80b2d30e15360d9e6
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-05-30 09:58:42 -0500

    prieba5

--- movimientos/views.py
+++ movimientos/views.py
@@ -581,7 +581,7 @@ def generarPDFGuia(request, id):
         pdf.drawString(14.5*cm, altura_pagina - 12.65*cm, str(' UND'))
     # #y = 110
     font_size = 9
-    pdf.setFont("Helvetica", font_size)
+    pdf.setFont("Bookman Old", font_size)
     x = 60
     h = 60
     f = 60
