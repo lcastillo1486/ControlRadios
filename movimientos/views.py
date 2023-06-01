75312e4e4173d2a42f7bc2078f0646776e4db2bc
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-05-31 19:34:26 -0500

    prueba540

--- movimientos/views.py
+++ movimientos/views.py
@@ -122,7 +122,7 @@ def generarSalida(request, id):
     try:
         detalle_salida = ordenRegistro.objects.get(id=id)
         msalida = salidasDetalle.objects.get(id_orden=id)
-        salida_id = msalida.id
+        #salida_id = msalida.id
         # if movimientoRadios.objects.filter(id_salida = salida_id).exists():
         #      cuenta_radios = movimientoRadios.objects.filter(id_salida = salida_id).count()
         #      serial_radios = movimientoRadios.objects.filter(id_salida = salida_id)
