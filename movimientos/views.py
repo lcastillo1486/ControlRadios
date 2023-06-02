8fb144119ae2113ca63ec7c0d04d2cad1f0d029e
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-06-02 10:36:38 -0500

    prueba239

--- movimientos/views.py
+++ movimientos/views.py
@@ -157,7 +157,7 @@ def generarSalida(request, id):
         if movimientoRadios.objects.filter(id_salida = salida_id).exists():
             cuenta_radios = movimientoRadios.objects.filter(id_salida = salida_id).count()
             serial_radios = movimientoRadios.objects.filter(id_salida = salida_id)
-            return render(request, 'salidaGenerada.html', {"salidaGenerada": msalida, "datosOrden": detalle_salida, 'formRadios': form, "cantidad_radios":cuenta_radios, "serial_radios":serial_radios})
+            return render(request, 'salidaGenerada.html', {"salidaGenerada": msalida, "datosOrden": detalle_salida, 'formRadios': form})
         
         return render(request, 'salidaGenerada.html', {"salidaGenerada": msalida, "datosOrden": detalle_salida, 'formRadios': form})
             
