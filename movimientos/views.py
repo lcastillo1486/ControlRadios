d504af7a986fddf7fae85f4115546270dd79ce55
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-06-02 16:56:55 -0500

    prueba245

--- movimientos/views.py
+++ movimientos/views.py
@@ -111,9 +111,11 @@ def ordenesDevueltas(request):
 
 def ordenDetalle(request, id):
     detalle = ordenRegistro.objects.get(id=id)
+    mochila_guardada = ""
     if mochila.objects.get(numero_orden=id).exists():
         mochila_guardada = mochila.objects.get(numero_orden=id)
-        return render(request, 'editarOrden.html', {"listaDetalles": detalle, "mochilaguardada":mochila_guardada})
+        color = mochila_guardada.color
+        return render(request, 'editarOrden.html', {"listaDetalles": detalle, "mochilaguardada":color})
     else:
         mochila_guardada = ""
     return render(request, 'editarOrden.html', {"listaDetalles": detalle, "mochilaguardada":mochila_guardada})
