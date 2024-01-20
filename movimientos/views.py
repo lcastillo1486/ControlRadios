6ac57322d5d1706900ad69fae3d34d252bff5e4a
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2024-01-19 22:11:57 -0500

    pruebanuevocampo1

--- movimientos/views.py
+++ movimientos/views.py
@@ -1165,9 +1165,9 @@ def generarPDFtotales(request):
             f'{anio}-01-01',
             f'{anio}-12-31' ))
 
-            agrupacion_por_mes = vista_movimiento_radios_tipos.objects.filter(fecha_normal__range=(
-            f'{anio}-01-01',
-            f'{anio}-12-31' )).annotate(mes=TruncMonth('fecha_normal')).values('mes').annotate(total_registros=Count('serialrx')).values('mes', 'total_registros')
+            # agrupacion_por_mes = vista_movimiento_radios_tipos.objects.filter(fecha_normal__range=(
+            # f'{anio}-01-01',
+            # f'{anio}-12-31' )).annotate(mes=TruncMonth('fecha_normal')).values('mes').annotate(total_registros=Count('serialrx')).values('mes', 'total_registros')
 
             # for i in result_busqueda:
             #     a = i.fecha_salida
