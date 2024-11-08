e0ebaa7fd2f7dc511da2360e50639f34ca16e528
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2024-11-07 20:59:39 -0500

    prueba05

--- movimientos/models.py
+++ movimientos/models.py
@@ -221,7 +221,6 @@ class vista_entrada_detalle(models.Model):
 
 class contable(models.Model):
 
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
     id_salida = models.PositiveIntegerField(null=False)
     id_orden = models.PositiveIntegerField(null=False)
     monto_base = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
