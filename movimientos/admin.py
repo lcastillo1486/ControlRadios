cc8ef6815c8943d86bf0581c97519fde4ae37337
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2024-12-05 13:01:08 -0500

    admin_site

--- movimientos/admin.py
+++ movimientos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import tipoRadios, invSeriales, estadoRadios
+from .models import tipoRadios, invSeriales, estadoRadios, CajasMikrot
 
 # Register your models here.
 
 admin.site.register(tipoRadios)
 admin.site.register(invSeriales)
 admin.site.register(estadoRadios)
+admin.site.register(CajasMikrot)
