547ebebd8fdc7efae589713dbae2775fbb224144
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-02-26 12:31:36 -0500

    prueba_sin_cod_manual

--- movimientos/views.py
+++ movimientos/views.py
@@ -3827,11 +3827,22 @@ def carga_formulario_pedido(request):
 
                 # telwhat = +51946364693
                 telwhat = ["+51914142952", "+51974616099"] 
-                mensaje = f'Hola. Tienes un nuevo pedido. Cliente: {nombre}\n Teléfono: {telefono}\n Dirección Entrega: {direccion}\n Comentarios del Cliente: {comentarios}\n *DATOS DEL PEDIDO:*\n Fecha de Entrega: {f_entrega}\n Fecha del Evento: {f_evento}\n Radios: {cantidad_rx}\n Cobras: {cantidad_cobras}\n Manos Libres: {cantidad_manos_l}\n Tipo Escolta: {cantidad_t_escolta}'
+                mensaje = f"""Hola. Tienes un nuevo pedido.
+Cliente: {nombre}
+Teléfono: {telefono}
+Dirección Entrega: {direccion}
+Comentarios del Cliente: {comentarios}\n
+*DATOS DEL PEDIDO:*
+Fecha de Entrega: {f_entrega}
+Fecha del Evento: {f_evento}
+Radios: {cantidad_rx}
+Cobras: {cantidad_cobras}
+Manos Libres: {cantidad_manos_l}
+Tipo Escolta: {cantidad_t_escolta}"""
                 for tel in telwhat:
                     url = "https://api.ultramsg.com/instance108195/messages/chat"
                     payload = f"token=uj605z2pvr8uws89&to=%2B{tel}&body={mensaje}"
-                    payload = payload.encode('utf8').decode('utf8')
+                    # payload = payload.encode('utf8').decode('utf8')
                     headers = {'content-type': 'application/x-www-form-urlencoded'}
                     response = requests.request("POST", url, data=payload, headers=headers)
 
@@ -3848,7 +3859,7 @@ Atte.
 
                 url = "https://api.ultramsg.com/instance108195/messages/chat"
                 payload = f"token=uj605z2pvr8uws89&to=%2B{telefono}&body={mensaje_cliente}"
-                payload = payload.encode('utf8').decode('utf8')
+                # payload = payload.encode('utf8').decode('utf8')
                 headers = {'content-type': 'application/x-www-form-urlencoded'}
                 response = requests.request("POST", url, data=payload, headers=headers)
 
