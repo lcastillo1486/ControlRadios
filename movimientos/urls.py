815f1507d992151e0be164bfc00638249fd42408
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2025-07-10 12:56:08 -0500

    media_url

--- movimientos/urls.py
+++ movimientos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from movimientos import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('entradas/<int:id>',views.entradas),
@@ -107,6 +108,6 @@ urlpatterns = [
 
 
 
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 ##recibir parametro 
